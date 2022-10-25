54a3fbb6509762f69e0e4efdad9a49b95a1f4c3b
Steven DeTeso <sadeteso@gmail.com>
2022-10-25 13:02:20 -0400

    Updated to Users_CRUD

--- users.py
+++ users.py
@@ -23,3 +23,19 @@ class User:
     def save(cls, data ):
         query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(email)s , NOW() , NOW() );" # type: ignore
         return connectToMySQL('users_schema').query_db( query, data ) 
+    
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(user_id)s;"
+        results = connectToMySQL('users_schema').query_db(query,data)
+        return results
+
+    @classmethod
+    def update_user(cls, data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s, updated_at = NOW() WHERE id = %(user_id)s"
+        return connectToMySQL('users_schema').query_db(query,data)
+    
+    @classmethod
+    def delete_user(cls,data):
+        query = "DELETE FROM users WHERE id = %(user_id)s"
+        return connectToMySQL('users_schema').query_db(query,data)
